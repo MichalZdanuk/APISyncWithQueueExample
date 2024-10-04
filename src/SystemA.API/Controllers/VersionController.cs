00aad1ca9e0dc2ced2de9313c6ce0f1edfc9b105
MichalZdanuk <zdanuk_m@wp.pl>
2024-10-04 21:12:35 +0200

    ✨ feat: Implement API version endpoint.

---
+++ src/SystemA.API/Controllers/VersionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SystemA.Application.DTOs;
+
+namespace SystemA.API.Controllers
+{
+    [Route("version")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public VersionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<APIVersionDto> GetVersion()
+        {
+            var version = new APIVersionDto
+            {
+                Version = _configuration["Version"]
+            };
+
+            return Ok(version);
+        }
+    }
+}
---
+++ src/SystemA.Application/DTOs/APIVersionDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SystemA.Application.DTOs
+{
+    public class APIVersionDto
+    {
+        public string Version { get; set; }
+    }
+}
